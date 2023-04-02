8331042cbdf6fd4efca8d41caca9a8b0f20ba526
Kasza Róbert Gábor <117159013+kazro34@users.noreply.github.com>
2023-04-02 08:49:53 +0200

    Update HAZI05.py

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -13,36 +13,34 @@ class KNNClassifier:
     def k_neighbors(self):
         return self.k
 
-    @staticmethod
+    
     def load_csv(csv_path:str) -> Tuple[pd.DataFrame , pd.DataFrame]:
-        dataset = pd.read_csv(csv_path, delimiter=',')
-        dataset =dataset.sample(frac=1, random_state=42).reset_index(drop=True)
-        x,y= dataset.iloc[:,:4], dataset.iloc[:,-1]
+        random.seed(42)
+        dataset = pd.read_csv(csv_path)
+        shuffled =dataset.sample(frac=1, random_state=42).reset_index(drop=True)
+        x,y= shuffled.iloc[:,:-1], shuffled.iloc[:,-1]
         return x,y
     
-    def train_test_split(self, features: pd.DataFrame, labels: pd.DataFrame) -> None:
-        
-        test_size = int(len(features) * self.test_split_ratio)
-        train_size = len(features) - test_size
+     def train_test_split(self, features: pd.DataFrame, labels: pd.DataFrame)-> None:
+        test_size = int(len(features)* self.test_split_ratio)
+        train_size= len(features)-test_size
+        assert len(features) == test_size + train_size, "Size mismatch"
 
-        assert len(features) == test_size + train_size, "Size mismatch!"
+        self.x_train, self.y_train = features.iloc[:train_size,:] ,labels.iloc[:train_size]
+        self.x_test, self.y_test = features.iloc[train_size:train_size+test_size,:] ,labels.iloc[train_size:train_size+test_size]
 
-        self.x_train = features[:train_size,:]
-        self.y_train = labels[:train_size]
-        self.x_test = features[train_size:train_size+test_size,:]
-        self.y_test = labels[train_size:train_size + test_size]
     
-    def euclidean(self, element_of_x: pd.DataFrame) -> pd.DataFrame:
-        return pd.sqrt(pd.sum((self.x_train - element_of_x)**2,axis=1))
+    def euclidean(self, element_of_x:pd.DataFrame)-> pd.DataFrame:
+        return (((self.x_train - element_of_x)**2).sum(axis=1)**0.5)
 
     def predict(self, x_test:pd.DataFrame) -> pd.DataFrame:
         labels_pred = []
         for x_test_element in x_test:
-            distances = self.euclidean(self.x_train,x_test_element)
+            distances = self.euclidean(x_test_element)
             distances = pd.DataFrame(sorted(zip(distances,self.y_train)))
-            label_pred = mode(distances[:self.k,1],keepdims=False).mode
+            label_pred = mode(distances.iloc[:self.k,1],keepdims=False).mode
             labels_pred.append(label_pred)
-        self.y_preds = pd.DataFrame(labels_pred,dtype=pd.int32)
+        self.y_preds = pd.DataFrame(labels_pred,dtype=pd.int64)
     
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
