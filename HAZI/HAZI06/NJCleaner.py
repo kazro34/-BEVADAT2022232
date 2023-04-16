8f31ca881c940ea82623f8671a2ec818fbd31072
Kasza Róbert Gábor <117159013+kazro34@users.noreply.github.com>
2023-04-16 18:12:19 +0200

    Add files via upload

--- HAZI/HAZI06/NJCleaner.py
+++ HAZI/HAZI06/NJCleaner.py
@@ -1,25 +1,59 @@
-import numpy as np
 import pandas as pd
 
 class NJCleaner():
-    def __init__(self, csv_path:str)-> None:
-        self.data =  pd.read_csv(csv_path)
+
+    def __init__(self, csv_path):
+        self.data = pd.read_csv(csv_path)
 
     def order_by_scheduled_time(self):
-        sorteddata = self.data.sort_values(by=['scheduled_time'])
-        return sorteddata
-        
-    
+        self.data = self.data.sort_values(by=['scheduled_time'])
+
+        return self.data
+
     def drop_columns_and_nan(self):
-        self.data.drop(columns=['from', 'to'])
-        self.data.dropna()
+        self.data = self.data.drop(columns=['from', 'to']).dropna()
+        return self.data
 
     def convert_date_to_day(self):
-        date = self.data['date'].to_list()
-        date = pd.Timestamp(self.data['date']).day_name()
-        self.data['day'] = date
-        self.data.drop(columns=['date'])
+        self.data['day'] = (pd.to_datetime(self.data['date'])).dt.day_name()
+        self.data = self.data.drop(columns=['date'])
 
-    #def convert_scheduled_time_to_part_of_the_day(self):
-        #for row in self.data.rows:
-            #if self.data['scheduled_time']
+        return self.data
+
+    def convert_scheduled_time_to_part_of_the_day(self):
+        self.data['part_of_the_day'] = pd.to_datetime(self.data['scheduled_time']).dt.hour.apply(
+            lambda time: 'early_morning' if time >= 4 and time < 8 else ('morning' if time >= 8 and time < 12 else (
+                'afternoon' if time >= 12 and time < 16 else ('evening' if time >= 16 and time < 20 else (
+                    'night' if time >= 20 and time < 24 else ('late_night'))))))
+        self.data = self.data.drop(columns=['scheduled_time'])
+        return self.data
+
+    def convert_delay(self):
+        self.data['delay'] = self.data['delay_minutes'].apply(lambda x: 0 if x >= 0 and x < 5 else (1))
+        return self.data
+
+    def drop_unnecessary_columns(self):
+        self.data = self.data.drop(columns=['train_id', 'actual_time', 'delay_minutes'])
+
+        return self.data
+
+    def save_first_60k(self, path):
+        self.data.head(60000).to_csv(path)
+
+    def prep_df(self, path='NJ.csv'):
+        self.order_by_scheduled_time()
+        self.drop_columns_and_nan()
+        self.convert_date_to_day()
+        self.convert_scheduled_time_to_part_of_the_day()
+        self.convert_delay()
+        self.drop_unnecessary_columns()
+        self.save_first_60k(path)
+
+data = NJCleaner('2018_03.csv')
+data.prep_df()
+
+
+
+
+
+
